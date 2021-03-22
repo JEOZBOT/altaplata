0de9890b207144e4182d1fcce7222f93fd147021
hiwaldo89 <oswaldo.zarate@wizeline.com>
2021-03-22 17:56:56 -0600

    change styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { StaticImage } from "gatsby-plugin-image";
 import { Modal, Container, Form, Button } from "react-bootstrap";
+import Seo from "../components/seo";
+import styled from "styled-components";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "@fontsource/roboto";
+
+const Wrapper = styled.div`
+  font-family: "Roboto", sans-serif;
+  .modal-content {
+    background: #f6f6f6;
+  }
+`;
 
 const Homepage = () => {
   const [show, setShow] = useState(false);
@@ -16,9 +26,10 @@ const Homepage = () => {
   }, []);
 
   return (
-    <>
+    <Wrapper>
+      <Seo title="AG Air life" />
       <div className="text-center">
-        <StaticImage src="../images/altaplata-3.jpg" alt="altaplata" />
+        <StaticImage src="../images/altaplata-3.jpg" alt="alta plata" />
       </div>
       <Modal
         show={show}
@@ -31,6 +42,14 @@ const Homepage = () => {
         <Modal.Header closeButton />
         <Modal.Body>
           <Container>
+            <StaticImage
+              src="../images/logo.png"
+              alt="alta plata"
+              width={100}
+            />
+            <h2 className="h4">
+              Déjanos tus datos para recibir más información
+            </h2>
             <Form name="contact" data-netlify="true">
               <Form.Group controlId="name">
                 <Form.Label>Nombre</Form.Label>
@@ -44,12 +63,14 @@ const Homepage = () => {
                 <Form.Label>Correo</Form.Label>
                 <Form.Control type="email" name="Correo" />
               </Form.Group>
-              <Button type="submit">Enviar</Button>
+              <Button type="submit" className="bg-dark border-0">
+                Enviar
+              </Button>
             </Form>
           </Container>
         </Modal.Body>
       </Modal>
-    </>
+    </Wrapper>
   );
 };
 
